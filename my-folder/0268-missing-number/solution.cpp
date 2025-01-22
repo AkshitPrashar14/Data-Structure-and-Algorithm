434b56b811c244bf2df56fe718bb9a29552466b1
Akshit Prashar <153202327+AkshitPrashar14@users.noreply.github.com>
2025-01-22 18:01:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21.9 MB (39.23%)

--- my-folder/0268-missing-number/solution.cpp
+++ my-folder/0268-missing-number/solution.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int a=nums.size();
-        int sum=(a*(a+1))/2;
+        int n=nums.size();
+        int b=n;
         int sum1=0;
-        for(int i=0;i<=a-1;i++){
-            sum1=sum1+nums[i];
+        while(b>0){
+            sum1+=b;
+            b--;
         }
-        int missing=sum-sum1;
-        return missing;
-    }
-};
+        int sum2=0;
+        for(int i=0;i<n;i++){
+            sum2+=nums[i];
+            }
+        return sum1-sum2;
+        }
+    };
