1c4155961b37ed6c1fa49da1997ba783a5e56437
Akshit Prashar <153202327+AkshitPrashar14@users.noreply.github.com>
2026-02-09 09:00:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 48.1 MB (47.62%)

---
+++ my-folder/0050-powx-n/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double myPow(double x, long n) {
+        long N=n;
+        if(N==0)return 1;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        double half=myPow(x,N/2);
+        if(N%2==0){
+            return half*half;
+        }else{
+            return half*half*x;
+        }
+    }
+}
