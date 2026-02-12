4e7305ffbf2d0d1f663e3e0b4a3ad25757d170df
Akshit Prashar <153202327+AkshitPrashar14@users.noreply.github.com>
2026-02-12 06:09:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 47.1 MB (82.73%)

--- my-folder/0268-missing-number/solution.java
+++ my-folder/0268-missing-number/solution.java
@@ -1,16 +1,24 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int sum1=0;
-        int sum2=0;
+        // int sum1=0;
+        // int sum2=0;
+        // int n=nums.length;
+        // for(int i=0;i<n;i++){
+        //     sum1+=nums[i];
+        // }
+        // while(n>0){
+        //     sum2+=n;
+        //     n--;
+        // }
+        // int sum=sum2-sum1;
+        // return sum;
         int n=nums.length;
+        int xor=0;
         for(int i=0;i<n;i++){
-            sum1+=nums[i];
+            xor=xor^i;
+            xor=xor^nums[i];
         }
-        while(n>0){
-            sum2+=n;
-            n--;
-        }
-        int sum=sum2-sum1;
-        return sum;
+        xor=xor^n;
+        return xor;
     }
 }
