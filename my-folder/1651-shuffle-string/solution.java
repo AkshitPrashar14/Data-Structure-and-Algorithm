70aa6836e18b81be8aa918551d26e4791dc1ef78
Akshit Prashar <153202327+AkshitPrashar14@users.noreply.github.com>
2026-03-16 06:21:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 45.8 MB (13.22%)

---
+++ my-folder/1651-shuffle-string/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        int len = s.length();
+        char[] res = new char[len];
+
+        for (int i = 0; i < len; i++) {
+            res[indices[i]] = s.charAt(i);
+        }
+
+        return new String(res);
+    }
+}
