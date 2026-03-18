423e8a28776481e0315f1638b41b73299c9b8bd1
Akshit Prashar <153202327+AkshitPrashar14@users.noreply.github.com>
2026-03-18 15:29:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.2 MB (15.83%)

--- my-folder/1013-fibonacci-number/solution.java
+++ my-folder/1013-fibonacci-number/solution.java
@@ -1,11 +1,13 @@
 class Solution {
     public int fib(int n) {
-        // int[] dp=new int[n+1];
-        // if(n<=1)return n;
-        // if(dp[n]!=0)return dp[n];
-        // dp[n]=fib(n-1)+fib(n-2);
-        // return dp[n];   #Using Dynamic Programming
         if(n<=1)return n;
-        return fib(n-1)+fib(n-2);
+        int a=0;
+        int b=1;
+        for(int i=2;i<=n;i++){
+            int c=a+b;
+            a=b;
+            b=c;
+        }
+        return b;
     }
 }
