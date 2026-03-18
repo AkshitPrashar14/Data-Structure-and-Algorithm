3026ab13b73997ca9980f0cc9e649a18680fe2ba
Akshit Prashar <153202327+AkshitPrashar14@users.noreply.github.com>
2026-03-18 15:25:20 +0000

    Sync LeetCode submission Runtime - 2 ms (99.72%), Memory - 43.7 MB (39.91%)

--- my-folder/1078-remove-outermost-parentheses/solution.java
+++ my-folder/1078-remove-outermost-parentheses/solution.java
@@ -1,20 +1,21 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        StringBuilder result = new StringBuilder();
-        int open = 0;
-
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                if (open > 0) result.append(c);
-                open++;
-            } else {
-                open--;
-                if (open > 0) result.append(c);
+        StringBuilder result=new StringBuilder();
+        int depth=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='('){
+                if(depth>0){
+                    result.append(ch);
+                }
+                depth++;
+            }
+            else{
+            depth--;
+            if(depth>0){
+                result.append(ch);
+            }
             }
         }
-
         return result.toString();
     }
 }
-
-
