3671329039f1d249e8d69060ea6bf776921dab25
Akshit Prashar <153202327+AkshitPrashar14@users.noreply.github.com>
2026-03-28 11:33:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.9 MB (65.31%)

---
+++ my-folder/1878-check-if-array-is-sorted-and-rotated/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int min=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]>nums[i])min++;
+        }
+        if(nums[0]<nums[nums.length-1])min++;
+        return min<=1;
+    }
+}
