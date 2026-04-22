fd19810426f97d1b6b7933702afa19e63345ce68
Akshit Prashar <153202327+AkshitPrashar14@users.noreply.github.com>
2026-04-22 13:28:33 +0000

    Sync LeetCode submission Runtime - 2 ms (100.00%), Memory - 46.3 MB (86.15%)

--- my-folder/0013-roman-to-integer/solution.java
+++ my-folder/0013-roman-to-integer/solution.java
@@ -1,31 +1,30 @@
 class Solution {
     public int romanToInt(String s) {
-        int sum=0;
-        for(int i=0;i<s.length();i++){
-            int curr=value(s.charAt(i));
-            if(i+1<s.length()){
-                int next=value(s.charAt(i+1));
-                if(curr<next){
-                    sum-=curr;
-                }else{
-                    sum+=curr;
-                }
-            }else{
-                sum+=curr;
+        int total = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int current = value(s.charAt(i));
+            
+            if (i < s.length() - 1 && current < value(s.charAt(i + 1))) {
+                total -= current;
+            } else {
+                total += current;
             }
         }
-        return sum;
+
+        return total;
     }
-    private int value(char c){
-        switch(c){
-            case 'I':return 1;
-            case 'V':return 5;
-            case 'X':return 10;
-            case 'L':return 50;
-            case 'C':return 100;
-            case 'D':return 500;
-            case 'M':return 1000;
+
+    private int value(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
         }
-        return 0;
     }
 }
