75432f93402a4f84dbee114c5e3b183934122aa8
Akshit Prashar <153202327+AkshitPrashar14@users.noreply.github.com>
2026-07-24 18:35:35 +0000

    Sync LeetCode submission Runtime - 6 ms (30.98%), Memory - 19.6 MB (93.92%)

---
+++ my-folder/0950-x-of-a-kind-in-a-deck-of-cards/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasGroupsSizeX(self, deck: List[int]) -> bool:
+        count = Counter(deck)
+        g = 0
+        for freq in count.values():
+            g = gcd(g, freq)
+
+        return g >= 2
